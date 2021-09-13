ffda23da0dde55c5a81c0c734285a6604e1cdd7c
ryley-matos <mail@ryley.io>
2021-09-12 21:30:47 -0700

    Added use of react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
@@ -11,7 +12,9 @@ const store = createStore(reducers, applyMiddleware(thunkMiddleware))
 
 const WrappedHome = () => (
   <Provider store={store}>
-    <Home />
+    <Router>
+      <Home />
+    </Router>
   </Provider>
 )
 
