a92da628ae8b0f1c7f8f06b8c237099a4f6ce783
ryley-matos <mail@ryley.io>
2021-09-12 19:42:23 -0700

    Added recipe fetching via id and implemented relevant error handling. Attached RecipeMiddleware to GET /api/recipe/:id

--- server/routes/recipe.ts
+++ server/routes/recipe.ts
@@ -1,5 +1,20 @@
+import mongoose from "mongoose"
 import { Request, Response, NextFunction } from "express"
+import { RecipeModel } from "../models"
 
 export const recipeMiddleware = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
   // TODO fetch and return a recipe
+  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const recipeId = mongoose.Types.ObjectId(req.params.id)
+    const recipe = await RecipeModel.findById(recipeId)
+    if (!recipe) {
+      res.status(404).send({ error: `Recipe (${recipeId}) not found`})
+    }
+    else {
+      res.send(recipe)
+    }
+  }
+  else {
+    res.status(400).send({ error: 'Invalid Object ID'})
+  }
 }
